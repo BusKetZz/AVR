6f98145bfc76449446caea120793df02df5cd262
Jakub Standarski <busketz2k@gmail.com>
2019-04-27 16:17:42 +0200

    New folder & file: C_Useful_Codes & dtostre.c - added

---
+++ C_Useful_Codes/dtostre.c
@@ -0,0 +1,33 @@
+/*
+ *  Function which converts number of type double (float as well) into string
+ *  in such a format:   [-]z.zzze±zz
+ * 
+ *  @function prototype   :   char* dtostre (double __val, char* __s, unsigned char __prec, unsigned char __flags);
+ * 
+ *  @param        __val   :   number to convert
+ *  @param        __s     :   pointer to buffer in which conversion result will be placed
+ *  @param        __prec  :   amount of fractional number
+ *  @param        __flags :   conversion parameters:
+ *                            - DTOSTRE_UPPERCASE   - E instead of e
+ *                            - DTOSTRE_ALWAYS_SIGN - space before positive numbers
+ *                            - DTOSTRE_PLUS_SIGN   - '+' will be added before positive numbers
+ * 
+ *  @return               :   buffer address in which conversion result resides (equal to address given in __s)
+ * 
+ *  @note                 :   it is important, that buffer, which stores result, has sufficient size
+ * 
+ */
+
+#include <stdlib.h>
+
+
+int main(void)
+{
+    char buffer[20];
+    float z = 12.345678;
+    dtostre(z, buffer, 2, 0);
+
+    return 0;
+}
+
+
