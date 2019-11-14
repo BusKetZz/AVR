574d638ff1d4dfbcebe87fd4cbf5a7a4056b790b
Jakub Standarski <busketz2k@gmail.com>
2019-11-14 21:27:29 +0100

    Workspace/Projects/ATmega168/Light_Sensor/Include/ADC.h: ADC0_Init() and ADC0_MakeSingleConversion() functions declarations added

--- Workspace/Projects/ATmega168/Light_Sensor/Include/ADC.h
+++ Workspace/Projects/ATmega168/Light_Sensor/Include/ADC.h
@@ -26,4 +26,22 @@
 /*                       PUBLIC FUNCTIONS DECLARATIONS                       */
 /*****************************************************************************/
 
+/**
+ * @brief Initialize ADC0 with /16 clock prescaler and AVCC reference voltage
+ * 
+ * @param void
+ * 
+ * @return void
+ */ 
+void ADC0_Init(void);
 
+
+
+/**
+ * @brief Make ADC0 single conversion (blocking mode)
+ * 
+ * @param void
+ * 
+ * @return void
+ */
+void ADC0_MakeSingleConversion(void); 
