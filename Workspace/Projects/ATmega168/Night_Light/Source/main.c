ab2247a3c199212589fab956def5e4c1220930ba
Jakub Standarski <busketz2k@gmail.com>
2019-11-16 12:44:10 +0100

    Workspace/Projects/ATmega168: Night_Light project added (WIP)

---
+++ Workspace/Projects/ATmega168/Night_Light/Source/main.c
@@ -0,0 +1,28 @@
+/**
+ * File: main.c
+ * 
+ * Author: Jakub Standarski
+ * 
+ * Date: 13.11.2019
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "ADC.h"
+#include "USART.h"
+
+
+
+int main(void)
+{
+    USART_Init(BAUD_9600);
+    ADC0_Init_Freerunning();
+
+    while(1)
+    {
+        USART_TransmitByte(ADCH);
+        _delay_ms(SAMPLE_DELAY);
+    }
+
+    return 0;
+}
