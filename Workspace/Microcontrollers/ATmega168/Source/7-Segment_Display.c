2f56c4643c6670dcc8326b95801e264d5cf40fe3
Jakub Standarski <busketz2k@gmail.com>
2019-11-17 19:48:44 +0100

    Workspace/Microcontrollers/ATmega168/Source: 7-Segment_Display.c source file added (WIP)

---
+++ Workspace/Microcontrollers/ATmega168/Source/7-Segment_Display.c
@@ -0,0 +1,68 @@
+/**
+ * File: 7-Segment_Display.c
+ * 
+ * Author: Jakub Standarski
+ * 
+ * Date: 17.11.2019
+ */
+
+#define __AVR_ATmega168__
+
+#include "7-Segment_Display.h"
+
+
+
+/*****************************************************************************/
+/*                          PRIVATE OWN DATA TYPES                           */
+/*****************************************************************************/
+
+
+
+/*****************************************************************************/
+/*                             PRIVATE DEFINES                               */
+/*****************************************************************************/
+
+/* 7-Segment display literal-pin equivalent */
+#define A   (1 << PIN0)
+#define B   (1 << PIN1)
+#define C   (1 << PIN2)
+#define D   (1 << PIN3)
+#define E   (1 << PIN4)
+#define F   (1 << PIN5)
+#define G   (1 << PIN6)
+#define H   (1 << PIN7)
+
+
+/* Numbers to display */
+#define ZERO    (A | B | C | D | E | F)
+#define ONE     (B | C)
+#define TWO     (A | B | G | E | D)
+#define THREE   (A | B | C | D | G)
+#define FOUR    (B | C | F | G)
+#define FIVE    (A | F | G | C | D)
+#define SIX     (A | F | E | D | C | G)
+#define SEVEN   (A | B | C)
+#define EIGHT   (A | B | C | D | E | F | G)
+#define NINE    (A | F | G | B | C | D)
+#define DOT     (H)
+
+
+
+/*****************************************************************************/
+/*                           PRIVATE VARIABLES                               */
+/*****************************************************************************/
+
+static const uint8_t numberToDisplay[10] = {ZERO, ONE, TWO, THREE, FOUR, FIVE,
+                                            SIX, SEVEN, EIGHT, NINE};
+
+
+
+/*****************************************************************************/
+/*                       PRIVATE FUNCTIONS DEFINITIONS                       */
+/*****************************************************************************/
+
+
+
+/*****************************************************************************/
+/*                       PUBLIC FUNCTIONS DEFINITIONS                        */
+/*****************************************************************************/
