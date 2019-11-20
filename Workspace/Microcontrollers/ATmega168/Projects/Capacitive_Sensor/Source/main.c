11edbc7f43ac24d3bd6f9267403d10282d72257a
Jakub Standarski <busketz2k@gmail.com>
2019-11-20 21:44:04 +0100

    Workspace/Microcontrollers/ATmega168/Projects/Capacitive_Sensor/Source/main.c: initial implementation of the program

---
+++ Workspace/Microcontrollers/ATmega168/Projects/Capacitive_Sensor/Source/main.c
@@ -0,0 +1,25 @@
+/**
+ * File: main.c
+ * 
+ * Project: Capacitive_Sensor
+ * 
+ * Author: Jakub Standarski
+ * 
+ * Date: 20.11.2019
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/power.h>
+#include <util/delay.h>
+#include "LED.h"
+#include "USART.h"
+
+
+
+int main(void)
+{
+
+
+    return 0;
+}
