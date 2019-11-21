93dbc3f3feded1a53a32779386eef9e84526bddf
Jakub Standarski <busketz2k@gmail.com>
2019-11-21 22:56:25 +0100

    Workspace/Microcontrollers/ATmega168/Source: Timer.c initial implementation (WIP)

---
+++ Workspace/Microcontrollers/ATmega168/Source/Timer.c
@@ -0,0 +1,30 @@
+/**
+ * File: Timer.c
+ * 
+ * Author: Jakub Standarski
+ * 
+ * Date: 21.11.2019
+ */
+
+#define __AVR_ATmega168__
+
+#include "Timer.h"
+
+
+
+/*****************************************************************************/
+/*                       PUBLIC FUNCTIONS DEFINITIONS                        */
+/*****************************************************************************/
+
+void Timer0_Init(uint8_t compareOutputMode, uint8_t clockPrescaler)
+{
+    /* TODO */
+}
+
+
+
+void Timer1_Init(uint8_t compareOutputMode, uint8_t clockPrescaler)
+{
+    
+}
+
