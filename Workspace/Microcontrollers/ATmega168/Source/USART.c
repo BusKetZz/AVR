4983d1fb9436000bd5b50932f195873462e055c1
Jakub Standarski <busketz2k@gmail.com>
2019-11-28 19:35:12 +0100

    Workspace/Microcontrollers/ATmega168/Source/USART.c: small modifications in USART_Init() function

--- Workspace/Microcontrollers/ATmega168/Source/USART.c
+++ Workspace/Microcontrollers/ATmega168/Source/USART.c
@@ -10,25 +10,26 @@
 
 #include "USART.h"
 
+#include <avr/io.h>
+
+
 
 
 /*****************************************************************************/
 /*                       PUBLIC FUNCTIONS DEFINITIONS                        */
 /*****************************************************************************/
 
-void USART_Init(uint16_t baudrate)
+void USART_Init(unsigned int baudRate)
 {
     /* Set baud rate */
-    UBRR0H |= (baudrate >> 8);
-    UBRR0L |= baudrate;
+    UBRR0H |= (unsigned char)(baudRate >> 8);
+    UBRR0L |= (unsigned char)baudRate;
 
     /* Enable receiver and transmitter */
-    UCSR0B |= ((1 << RXEN0) | (1 << TXEN0));
+    UCSR0B = ((1 << RXEN0) | (1 << TXEN0));
 
     /* Set frame format: 8 data bits, 1 stop bit, no parity */
-    UCSR0B &= ~(1 << UCSZ02);
-    UCSR0C |= ((1 << UCSZ00) | (1 << UCSZ01));
-    UCSR0C &= ~((1 << USBS0) | (1 << UPM00) | (1 << UPM01));   
+    UCSR0C = ((1 << UCSZ01) | (1 << UCSZ00));
 }
 
 
