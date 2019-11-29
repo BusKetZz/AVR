6c440c728bd490a4249874dd60b5c595d7ac7a88
Jakub Standarski <busketz2k@gmail.com>
2019-11-29 22:16:17 +0100

    Workspace/Microcontrollers/ATmega168/Source/USART.c: RXEN and TXEN bit setting moved to the end of USART_Init() function

--- Workspace/Microcontrollers/ATmega168/Source/USART.c
+++ Workspace/Microcontrollers/ATmega168/Source/USART.c
@@ -20,13 +20,13 @@
 
 void USART_Init(unsigned int baudRate)
 {
+    /* Set baud rate */
+    UBRR0H = (unsigned char)(baudRate >> 8);
+    UBRR0L = (unsigned char)baudRate;
+
     CLEAR_BIT(UCSR0A, U2X0);       /* Double Transmission Speed disabled     */
     CLEAR_BIT(UCSR0A, MPCM0);      /* Multi-processor Communication Mode  
                                       disabled                               */
-
-    SET_BIT(UCSR0B, RXEN0);        /* Receiver enabled                       */
-    SET_BIT(UCSR0B, TXEN0);        /* Transmitter enabled                    */
-    CLEAR_BIT(UCSR0B, UCSZ02);     /* 8-bit Character Size                   */
 
     CLEAR_BIT(UCSR0C, UMSEL00);    /* Asynchronous USART Mode                */
     CLEAR_BIT(UCSR0C, UMSEL01);    /* Asynchronous USART Mode                */
@@ -36,9 +36,9 @@ void USART_Init(unsigned int baudRate)
     SET_BIT(UCSR0C, UCSZ01);       /* 8-bit Character Size                   */
     SET_BIT(UCSR0C, UCSZ00);       /* 8-bit Character Size                   */
 
-    /* Set baud rate */
-    UBRR0H = (unsigned char)(baudRate >> 8);
-    UBRR0L = (unsigned char)baudRate;
+    CLEAR_BIT(UCSR0B, UCSZ02);     /* 8-bit Character Size                   */
+    SET_BIT(UCSR0B, RXEN0);        /* Receiver enabled                       */
+    SET_BIT(UCSR0B, TXEN0);        /* Transmitter enabled                    */
 }
 
 
